d1851f99b85fa2669c5b10fcabe17b81451e88f0
Miyuki <listencpp@gmail.com>
2020-12-31 02:11:43 +0800

    Initial Commit

---
+++ Translator-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Translator-iOS
+//
+//  Created by Zhixun Liu on 2020/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
